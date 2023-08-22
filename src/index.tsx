b8d0808565910fe35a5b21fb2c8f9fd89702fd10
BEYOND-224 <darkcosvisuals@gmail.com>
2023-08-22 19:29:40 +0530

    chat visibility 👀

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
-import React, { PureComponent, CSSProperties, RefObject } from 'react';
+import React, { PureComponent, CSSProperties, RefObject, ReactNode } from 'react';
 import { Client } from '@widgetbot/embed-api';
 import { Embed, Root } from './elements';
 import { generateUUID, searchParams } from './util';
 
+interface Props {
+  server?: string;
+  channel?: string;
+  shard?: string;
+  username?: string;
+  avatar?: string;
+  token?: string;
+  notifications?: boolean;
+  notificationTimeout?: number;
+  accessibility?: string[];
+  settingsGroup?: string;
+  defer?: boolean;
+  className?: string;
+  onAPI?: (api: Client) => void;
+  style?: CSSProperties;
+  height?: number | string;
+  width?: number | string;
+  focusable?: boolean;
+  options?: Record<string, string>;
+  showChatButtonLabel?: string;
+  hideChatButtonLabel?: string;
+}
 interface Props {
   server?: string;
   channel?: string;
@@ -30,9 +52,18 @@ interface State {
   url: string | null;
   id: string;
   isChatVisible: boolean;
-  hasError: boolean;
 }
 
+class WidgetBot extends PureComponent<Props, State> {
+  static defaultProps: Props = {
+    server: '299881420891881473',
+    shard: 'https://e.widgetbot.io',
+    options: {},
+    defer: false,
+    focusable: true,
+    showChatButtonLabel: 'Show Chat',
+    hideChatButtonLabel: 'Hide Chat',
+  };
 class WidgetBot extends PureComponent<Props, State> {
   static defaultProps: Props = {
     server: '299881420891881473',
@@ -48,19 +79,23 @@ class WidgetBot extends PureComponent<Props, State> {
     url: null,
     id: generateUUID(),
     isChatVisible: true,
-    hasError: false,
   };
 
   api: Client = new Client({
     id: this.state.id,
     iframe: null as RefObject<HTMLIFrameElement> | null,
   });
+  api: Client = new Client({
+    id: this.state.id,
+    iframe: null as RefObject<HTMLIFrameElement> | null,
+  });
 
-  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
-    console.error('An error occurred in WidgetBot:', error, errorInfo);
-    this.setState({
-      hasError: true,
-    });
+  static getDerivedStateFromProps(props: Props, state: State): Partial<State> | null {
+    // ... (remaining code remains the same)
+  }
+
+  componentDidMount() {
+    // ... (remaining code remains the same)
   }
 
   toggleChatVisibility = (): void => {
@@ -80,12 +115,7 @@ class WidgetBot extends PureComponent<Props, State> {
       showChatButtonLabel,
       hideChatButtonLabel,
     } = this.props;
-    const { isChatVisible, hasError } = this.state;
-
-    if (hasError) {
-      // Display an error message or component
-      return <div>Sorry, something went wrong.</div>;
-    }
+    const { isChatVisible } = this.state;
 
     return (
       <div>
@@ -107,6 +137,28 @@ class WidgetBot extends PureComponent<Props, State> {
       </div>
     );
   }
+    return (
+      <div>
+        <div className={className} style={{ ...Root({ width, height }), ...style }}>
+          {isChatVisible && (
+            <iframe
+              src={defer ? '' : this.state.url}
+              ref={(ref) => (this.api.iframe = ref)}
+              style={Embed}
+              tabIndex={focusable ? undefined : -1}
+              allow="clipboard-write; fullscreen"
+              title="Discord chat embed"
+            />
+          )}
+        </div>
+        <button onClick={this.toggleChatVisibility}>
+          {isChatVisible ? hideChatButtonLabel : showChatButtonLabel}
+        </button>
+      </div>
+    );
+  }
 }
+
+export default WidgetBot;
 
 export default WidgetBot;
